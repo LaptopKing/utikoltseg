fd51163a2809637598a33356af4f2e909c858667
LaptopKing <csordaspeter2002@gmail.com>
2022-02-21 12:41:00 +0100

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -11,9 +11,26 @@ function uzemanyag() {
 }
 
 function ellenorzes() {
+	var nev = document.getElementById("nev").value;
+	var lakcim = document.getElementById("lakcim").value;
+	var szul = document.getElementById("szul-date").value;
+	var gyartmany = document.getElementById("nev").value;
+	var utazas = document.getElementById("utazas-date").value;
+	var kiindulas = document.getElementById("kiindulas").value;
+	var uticel = document.getElementById("uticel").value;
+	var tovabbi = document.getElementById("tovabbi-uticel").value;
+	var tavolsag = document.getElementById("tavolsag").value;
 
+	if (nev == "" || lakcim == "" || szul == "" || gyartmany == "" || utazas == "" || kiindulas == "" || uticel == "" || tavolsag == "")
+	{
+		alert("Kérem adjon meg minden szükséges adatot!");
+	}
+	else {
+		var utikoltseg = Math.round(document.getElementByID("fogyasztas").value * document.getElementById("egysegar").value * tavolsag / 100);
+		var ertekcsokkenes = tavolsag * 15;
+	}
 }
 
 function nyomtatas() {
-	
+
 }
