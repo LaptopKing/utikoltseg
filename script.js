b0639358e592a3d644d5b23d106aac0924f4a72c
LaptopKing <csordaspeter2002@gmail.com>
2022-02-21 12:19:44 +0100

    modified:   index.html 	modified:   script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function uzemanyag() {
+	var fajta = document.getElementById("fajta");
+	if (fajta.value == "Benzin")
+	{
+		document.getElementById("egysegar").value = 478;
+	}
+	else if (fajta.value == "Dizel")
+	{
+		document.getElementById("egysegar").value = 518;
+	}
+}
